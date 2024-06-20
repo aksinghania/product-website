12ed2a00e968565ae03192ab35e2b643ad1634f4
aksinghania <akshtsinghnia@gmail.com>
2024-06-21 01:50:50 +0530

    partial navigation bar for videos in product section

--- src/components/VideoCarousel.jsx
+++ src/components/VideoCarousel.jsx
@@ -14,7 +14,19 @@ const VideoCarousel = () => {
     isPlaying: false,
   });
 
+  const [loadedData, setloadedData] = useState([]);
+
   const { isEnd, isLastVideo, startPlay, videoId, isPlaying } = video;
+
+  useEffect(() => {
+    if (loadedData.length > 3) {
+      if (!isPlaying) {
+        videoRef.current[videoId].pause();
+      } else {
+        startPlay && videoRef.current[videoId].play();
+      }
+    }
+  }, [startPlay, videoId, isPlaying, loadedData]);
 
   useEffect(() => {
     const currentProgress = 0;
@@ -22,6 +34,11 @@ const VideoCarousel = () => {
 
     if (span[videoId]) {
       // animate the progress of the video
+      let anim = gsap.to(span[videoId], {
+        onUpdate: () => {},
+
+        onComplete: () => {},
+      });
     }
   }, [videoId, startPlay]);
 
@@ -32,7 +49,19 @@ const VideoCarousel = () => {
           <div key={list.id} id="slider" className="sm:pr-20 pr-10">
             <div className="video-carousel_container">
               <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
-                <video id="video" playsInline={true} preload="auto" muted>
+                <video
+                  id="video"
+                  playsInline={true}
+                  preload="auto"
+                  muted
+                  ref={(el) => (videoRef.current[i] = el)}
+                  onPlay={() => {
+                    setVideo((prevVideo) => ({
+                      ...prevVideo,
+                      isPlaying: true,
+                    }));
+                  }}
+                >
                   <source src={list.video} type="video/mp4" />
                 </video>
               </div>
@@ -47,6 +76,18 @@ const VideoCarousel = () => {
             </div>
           </div>
         ))}
+      </div>
+
+      <div className="relative flex-center mt-10">
+        <div className="flex-center py-5 px-7 bg-gray-300 backdrop-blur rounded-full">
+          {videoRef.current.map((_, i) => (
+            <span
+              key={i}
+              ref={(el) => (videoDivRef.current[i] = el)}
+              className="mx-2 w-3 h-3 bg-gray-200"
+            ></span>
+          ))}
+        </div>
       </div>
     </>
   );
