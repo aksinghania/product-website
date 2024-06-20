38f19acc1a25e7e9f0fa98f1a29649df260dfd9b
aksinghania <akshtsinghnia@gmail.com>
2024-06-21 00:04:58 +0530

    section header

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,6 +1,24 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const Highlights = () => {
-    return (
-        <div className="text-white">Highlights</div>
-    )
-}
-export default Highlights
+  useGSAP(() => {
+    gsap.to("#title", { opacity: 1, y: 0 });
+  }, []);
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Highlights;
