b08f371a1ec075eb4868e574b45a21dbe2274acb
aksinghania <akshtsinghnia@gmail.com>
2024-06-21 00:51:20 +0530

    partially added carousel and added next section in home page

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,11 +1,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { rightImg, watchImg } from "../utils";
+import VideoCarousel from "./VideoCarousel";
 
 const Highlights = () => {
   useGSAP(() => {
     gsap.to("#title", { opacity: 1, y: 0 });
-    gsap.to(".link", { opacity: 1, y: 0 });
+    gsap.to(".link", { opacity: 1, y: 0, duration: 1, stagger: 0.25 });
   }, []);
 
   return (
@@ -30,6 +31,8 @@ const Highlights = () => {
             </p>
           </div>
         </div>
+
+        <VideoCarousel />
       </div>
     </section>
   );
