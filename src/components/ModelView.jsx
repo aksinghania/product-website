7b1b3eb0f3dc2a0dcbef1a07fa4f5cd4a7e8bbe2
aksinghania <akshtsinghnia@gmail.com>
2024-06-22 01:41:26 +0530

    imported the product on website now coding the integration

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,4 +1,8 @@
-import { PerspectiveCamera, View } from "@react-three/drei";
+import { OrbitControls, PerspectiveCamera, View } from "@react-three/drei";
+import Lights from "./Lights";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
+import * as THREE from "three";
 
 const ModelView = ({
   index,
@@ -13,15 +17,34 @@ const ModelView = ({
     <View
       index={index}
       id={gsapType}
-      className={`border-2 border-red-500 w-full h-full ${
-        index === 2
-      } ? "right-[-100%]" : ""
+      className={`w-full h-full ${index === 2} ? "right-[-100%]" : ""
       `}
     >
       {/* Ambient Light */}
       <ambientLight intensity={0.3} />
 
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      <Lights />
+
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.4}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotationState(controlRef.current.getAzimuthaAngle())}
+      />
+
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<div>Loading</div>}>
+          <IPhone />
+        </Suspense>
+      </group>
     </View>
   );
 };
