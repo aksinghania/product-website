6397e485b6956a62dee58dd98bafc0a0fa15f446
aksinghania <akshtsinghnia@gmail.com>
2024-06-23 05:05:43 +0530

    added animation to the image

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
+import HowItWorks from "./components/HowItWorks";
 import Features from "./components/Features";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Highlights />
       <Model />
       <Features />
+      <HowItWorks />
     </main>
   );
 };
