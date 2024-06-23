dee2e342cf5dd6e7ba52059e8c4ef6b11834a490
aksinghania <akshtsinghnia@gmail.com>
2024-06-23 06:16:56 +0530

    continuing with footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="py-5 sm:px-10 px-10">
+      <div className="screen-max-width">
+        <div>
+          <p className="font-semibold text-gray text-xs">
+            More ways to shop:{" "}
+            <span className="underline text-blue">Find an Apple Store </span>,
+            or <span className="underline text-blue">other retailer</span>, near
+            you.
+          </p>
+          <p className="font-semibold text-gray text-xs">
+            Or call 000800-040-1966
+          </p>
+        </div>
+
+        <div className="bg-neutral-700 my-5 h-[1px] w-full" />
+      </div>
+    </footer>
+  );
 };
 export default Footer;
